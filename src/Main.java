ce871a4bb0aab16fb98193731ab0d601a20e4a68
Caio Jesus <caioj3505@gmail.com>
2024-07-31 18:49:09 -0300

    Add find element method

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
-f//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Vector vector = new Vector(5);
+        Vector vector = new Vector(7);
+        vector.add("1");
+        vector.add("2");
+        vector.add("3");
+        vector.add("4");
+        vector.add("5");
+
+        System.out.println(vector.length());
+        System.out.println(vector.toString());
+        System.out.println(vector.find(3));
+        System.out.println(vector.find("1"));
+        System.out.println(vector.find("a"));
     }
 }
--- src/Vector.java
+++ src/Vector.java
@@ -1,7 +1,46 @@
+import java.util.Arrays;
+
 public class Vector {
     private String[] items;
+    private int length;
 
     public Vector(int length){
-        this.items = new String[length];
+        items = new String[length];
+        this.length = 0;
+    }
+
+    public boolean add(String item) {
+        if(length < items.length){
+            items[length] = item;
+            length++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String find(int position){
+        if(position >= 0 && position < length){
+            return items[position];
+        }else{
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public int find(String item){
+        for (int i=0; i < length; i++){
+            if(items[i].equals(item)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int length(){
+        return this.length;
+    }
+
+    public String toString(){
+        return Arrays.toString(items);
     }
 }
